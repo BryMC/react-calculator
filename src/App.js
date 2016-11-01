2f3ba006ea2c1394aaae20c44b08bdb209cedcba
Bryony McLaughlin <bryonymc@gmail.com>
2016-11-01 10:30:23 -0500

    started, got stuck

--- src/App.js
+++ src/App.js
@@ -3,10 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-
+constructor(props) {
+  super()
+  this.state = {
+    results: 0
+  }
+}
+handleSubmit(e){
+  this.setState({
+    results: this.state.results
+  })
+}
   render() {
+    let results =
+    this.props.results((a, b)) => (
+      <Result body=({a}+{b}) />
+    )
     return (
-      <Calculator />
+      <div className="container">
+        <h1>Add with React!</h1>
+        <div className="add">
+          <input type="text" />
+            <span>+</span>
+          <input type="text" />
+            <span>=</span>
+          <button onClick={(e) => this.handleSubmit(e)} type="submit"> Calculate! </button>
+          <h3>Addition results go here!</h3>
+        </div>
+      </div>
     );
   }
 }
